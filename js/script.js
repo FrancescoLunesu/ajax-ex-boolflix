ccdc483ca737b90d5d71f2093c60104b484b23b6
FrancescoLunesu <francesco.lunesu@outlook.it>
2020-09-02 14:10:52 +0200

    Secondo commit: ricerca tramite barra e stampa a video risultato

--- js/script.js
+++ js/script.js
@@ -15,6 +15,9 @@ $(document).ready(function(){
     // al click del bottone memorizzo nella variabile ricerca il valore dell'input con id "search"
     $("button").click(function(){
         var ricerca = $("#search").val();
+        console.log(ricerca);
+        // ad ogni click "svuoto" il mio ul in modo tale che mi cancelli eventuali risultati dati in precedenza
+    $("#n").empty();
 
         $.ajax(
             {
@@ -23,12 +26,26 @@ $(document).ready(function(){
                 data:{
                     api_key: "edf3f73f0278fbd70155fcad20871afb",
                     language: "it-IT",
-                    query: ricerca,
+                    query: ricerca
                 },
                 success: function(risposta){
-                    for (var i = 0; risposta.results.length; i++){
+                    // avevo scordato una "i" nelle condizioni del ciclo for e continuava ad andare in loop bloccando la pagina
+                    for (var i = 0; i < risposta.results.length; i++){
+                        var source = document.getElementById("entry-template").innerHTML;
+                        var template = Handlebars.compile(source);
+
+
+                        var context = {
+                            title: risposta.results[i].title,
+                            original_title: risposta.results[i].original_title,
+                            original_language: risposta.results[i].original_language,
+                            vote_average: risposta.results[i].vote_average
+                        };
+                        var html = template(context);
+                        $("#n").append(html);
 
                     }
+
                 },
                 error: function(){
                     alert("Attenzione! Si è verificato un errore");
