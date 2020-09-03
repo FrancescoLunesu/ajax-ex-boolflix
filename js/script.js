263d650ff661076f6916454f4e28d122a3b9a564
FrancescoLunesu <francesco.lunesu@outlook.it>
2020-09-03 18:29:56 +0200

    Quarto commit: bandiera per film con linga ita o eng

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// milestone 1:
 // Creare un layout base con una searchbar (una input e un button)
 // in cui possiamo scrivere completamente o parzialmente
 // il nome di un film. Possiamo, cliccando il bottone,
@@ -10,6 +11,19 @@
 // Lingua
 // Voto
 
+// milestone 2:
+// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5,
+//  così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,
+//   lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente,
+//  gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API
+//   (le flag non ci sono in FontAwesome).
+// Allarghiamo poi la ricerca anche alle serie tv.
+// Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query,
+//  sia le serie tv, stando attenti ad avere alla fine dei valori simili
+//  (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+
 
 $(document).ready(function(){
     // al click del bottone memorizzo nella variabile ricerca il valore dell'input con id "search"
@@ -19,6 +33,8 @@ $(document).ready(function(){
         ricercaFilm(ricerca);
         // ad ogni click "svuoto" il mio ul in modo tale che mi cancelli eventuali risultati dati in precedenza
     $("#n").empty();
+
+    });
 
 
 
@@ -37,7 +53,7 @@ function ricercaFilm(query){
                     success: function(risposta){
                         if(risposta.total_results > 0){
                             // se la chiamata dell'API restituisce uno o più risultato allora richiamo la funzione film() altrimenti la funzione nessunRisultato
-                            film(risposta);
+                            film(risposta.results);
                         } else {
                             nessunRisultato();
                         }
@@ -55,17 +71,15 @@ function ricercaFilm(query){
         alert("Nessun risultato trovato");
     }
 
-    function film(risposta){
-        for (var i = 0; i < risposta.results.length; i++){
+    function film(data){
+        for (var i = 0; i < data.length; i++){
             var source = document.getElementById("entry-template").innerHTML;
             var template = Handlebars.compile(source);
-
-
             var context = {
-                title: risposta.results[i].title,
-                original_title: risposta.results[i].original_title,
-                original_language: risposta.results[i].original_language,
-                vote_average: risposta.results[i].vote_average
+                title: data[i].title,
+                original_title: data[i].original_title,
+                original_language: bandiera(data[i].original_language),
+                vote_average: rate(data[i].vote_average),
             };
             var html = template(context);
             $("#n").append(html);
@@ -73,7 +87,37 @@ function ricercaFilm(query){
         }
     }
 
+    // creo una funzione per trasformare il voto da 1 a 10 decimale in voto da 1 a 5 interno
+    function rate(numero){
+        var divisione = numero / 2; //divido per due il Voto
+        // arrotondo il voto diviso per due
+        var nArrotondato = Math.ceil(divisione);
+        var star = " ";
+        // ciclo for per la creazione di 5 stelle
+        for (var i = 1; i<6; i++){
+            if(i <= nArrotondato){
+                // aggiungo la classe "stella" alle prime n stelle
+                star += '<i class="fas fa-star stella"></i>';
+            } else {
+                star += '<i class="fas fa-star"></i>';
+            };
+        };
+        return star;
+    };
 
-    });
+    function bandiera(flag){
+        var band = "";
+        if (flag == "it"){
+            band += '<img class="bandiere" src="img/it.svg" alt="ita">';
+        } else if (flag == "en"){
+            band += '<img class="bandiere" src="img/en.svg" alt="eng">';
+        } else {
+            band = flag;
+        };
+        return band;
+    };
+
+
+
 
 });
